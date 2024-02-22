61817689cf2badbb3e6572cd18d1431a84379660
Wai Yan Hein <wyan00196@gmail.com>
2024-02-22 16:43:02 +0630

    remove update

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// import Swal from "sweetalert2";
 import "./input.css"
 import Invoice from "./src/js/Invoice.js";
 
@@ -5,3 +6,28 @@ const invoice = new Invoice();
 invoice.init();
 
 // console.log(printBtn);
+
+// Swal.fire("Hello")
+// Swal.fire({
+//     title: "My Title",
+//     text: "San Kyi Tar Par",
+//     icon: "success"
+// })
+
+// Swal.fire({
+//     title: "Are you sure?",
+//     text: "You won't be able to revert this!",
+//     icon: "warning",
+//     showCancelButton: true,
+//     // confirmButtonColor: "#3085d6",
+//     // cancelButtonColor: "#d33",
+//     confirmButtonText: "Yes, delete it!"
+//   }).then((result) => {
+//     if (result.isConfirmed) {
+//       Swal.fire({
+//         title: "Deleted!",
+//         text: "Your file has been deleted.",
+//         icon: "success"
+//       });
+//     }
+//   });
--- src/js/core/record.js
+++ src/js/core/record.js
@@ -1,3 +1,4 @@
+import Swal from "sweetalert2";
 import { rowGroup, rowTemplate, totalCost } from "./selectors.js"
 
 // let recordIndex = 1;
@@ -23,10 +24,44 @@ export const updateRecordTotal = () => {
 
 export const deleteRecord = (event) => {
      const row = event.target.closest('.row')
-     if(confirm("Are u sure want to delete?")){
-        row.remove();
-        // updateRecordTotal();
-     }
+    //  if(confirm("Are u sure want to delete?")){
+    //     row.remove();
+    //     // updateRecordTotal();
+    //  }
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        // confirmButtonColor: "#3085d6",
+        // cancelButtonColor: "#d33",
+        confirmButtonText: "Confirm"
+      }).then((result) => {
+        if (result.isConfirmed) {
+            row.remove();
+            // Swal.fire({
+            //     title: "Deleted!",
+            //     text: "Your file has been deleted.",
+            //     icon: "success",
+            //     confirmButtonText: "Close",
+            // });
+            const Toast = Swal.mixin({
+                toast: true,
+                position: "bottom-start",
+                showConfirmButton: false,
+                timer: 2000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                  toast.onmouseenter = Swal.stopTimer;
+                  toast.onmouseleave = Swal.resumeTimer;
+                }
+              });
+              Toast.fire({
+                icon: "success",
+                title: "Remove successfully"
+              });
+        }
+    });
 }
 
 export const updateRecord = (productId,q) => {
